26f8e653e471d0904d66fcd565277c9afbb1d928
Tasso <tassobarbosa@gmail.com>
2021-04-17 13:19:23 -0300

    Comçando um aplicativo real

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button
+} from 'react-native';
+
+export default function App () {
+  return (
+    <View>
+      <View>
+        <TextInput placeholder="Course goal"/>
+        <Button title="Add" />
+      </View>
+      <View>
+        
+      </View>
+     
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
